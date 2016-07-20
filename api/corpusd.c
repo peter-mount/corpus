5208f906f626e015a81a254633ec3649769e3455
peter <peter@europa>
2016-07-20 19:38:53 +0100

    Enable webserver

---
+++ api/corpusd.c
@@ -0,0 +1,119 @@
+
+/* 
+ * File:   corpus-import.c
+ * Author: peter
+ *
+ * Created on 17 July 2016, 21:20
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <area51/json.h>
+#include <area51/log.h>
+#include <networkrail/corpus.h>
+#include <networkrail/corpus/import.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
+#include <sys/mman.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <networkrail/corpus/version.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include <area51/webserver.h>
+
+int verbose = 0;
+char *database = NULL;
+void *dbfile;
+
+static int about() {
+    logconsole("Usage: corpusd [-p{port}] database\n");
+    return EXIT_FAILURE;
+}
+
+static int parseargs(int argc, char** argv) {
+    for (int i = 1; i < argc; i++) {
+        char *s = argv[i];
+        if (s[0] == '-' && s[1] != 0) {
+            if (strncmp("-ip4", s, 4) == 0)
+                webserver_enableIPv4();
+            else if (strncmp("-ip6", s, 4) == 0)
+                webserver_enableIPv6();
+            else switch (s[1]) {
+                    case 'p':
+                        if (s[2])
+                            webserver.port = atoi(&s[2]);
+                        break;
+                        
+                    case 'v':
+                        verbose++;
+                        break;
+                        
+                    default:
+                        logconsole("Unsupported option %s", s);
+                        return about();
+                }
+        } else if (s[0]) {
+            if (database) {
+                return about();
+            }
+            database = s;
+        }
+    }
+
+    if (webserver.port < 1 || webserver.port > 65535 || !database)
+        return about();
+
+    return 0;
+}
+
+static int opendb() {
+    logconsole("Reading %s", database);
+
+    int fsock = open(database, O_RDONLY);
+    if (fsock == -1) {
+        logconsole("No source %s", database);
+        return EXIT_FAILURE;
+    }
+
+    struct stat sb;
+
+    if (fsock == -1 || fstat(fsock, &sb) == -1) {
+        return EXIT_FAILURE;
+    }
+
+    dbfile = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fsock, 0);
+    if (dbfile == MAP_FAILED) {
+        return EXIT_FAILURE;
+    }
+    return 0;
+}
+
+int main(int argc, char** argv) {
+
+    webserver_initialise();
+
+    int rc = parseargs(argc, argv);
+    if (rc)return rc;
+
+    logconsole(PKGBANNER);
+    logconsole(PKGBUILD);
+
+    rc = opendb();
+    if (rc)return rc;
+
+
+
+    logconsole("Starting webserver on port %d", webserver.port);
+    webserver_start();
+
+    while (1) {
+        sleep(60);
+    }
+}
+
